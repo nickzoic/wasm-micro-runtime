a08e97445f0b99f688bb788609ecc1378a75aa3d
Nick Moore <nick@zoic.org>
2020-02-05 13:11:54 +1100

    Building an ESP-IDF runtime for WAMR

---
+++ product-mini/platforms/esp-idf/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# from ESP-IDF 4.0 examples/build_system/cmake/idf_as_lib
+rm -rf build && mkdir build && cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=$IDF_PATH/tools/cmake/toolchain-esp32.cmake
+cmake --build .
