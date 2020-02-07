e65dafe812d909b0a33fa7abdd12158f388c3ca6
Nick Moore <nick@zoic.org>
2020-02-07 17:08:41 +1100

    fix up test build options

--- product-mini/platforms/esp-idf/test/build.sh
+++ product-mini/platforms/esp-idf/test/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-clang-8 --target=wasm32 -O3 -z stack-size=4096 -nostdlib test.c -Wl,--allow-undefined -Wl,--export=main, -Wl,--no-threads,--strip-all,--no-entry -o test.wasm
+clang-8 --target=wasm32 -O3 -z stack-size=4096 -Wl,--initial-memory=65536 --sysroot=../../../../wamr-sdk/app/libc-builtin-sysroot -Wl,--allow-undefined -Wl,--export=main, -Wl,--no-threads,--strip-all,--no-entry -nostdlib -o test.wasm test.c
 (
     echo // GENERATED FROM $PWD/test.c
     echo // `date -Iseconds`
